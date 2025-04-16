bc262644e225d5db8d87c09885dcf679a36606e8
DylanRothbauer <djrothbauer@gmail.com>
2025-04-16 11:42:13 -0500

    Updates to projectile

--- Ball Survival/Assets/Scripts/Projectile.cs
+++ Ball Survival/Assets/Scripts/Projectile.cs
@@ -4,8 +4,15 @@ using UnityEngine;
 
 public class Projectile : PowerUp
 {
+    public GameObject projectilePrefab;
+
+    private float speed = 5.0f;
+    
     public override void Activate(Collision collision)
     {
         Debug.Log("PROJECTILE ACTIVATE");
+
+        // Instanciate bullets to go in all directions
+        //Instantiate(projectilePrefab, gameObject.transform.position, projectilePrefab.transform.rotation);
     }
 }
